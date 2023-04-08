16abda729a7827847d69d5d0d25d030a9479a0d6
Глеб <lis.gleb2012@gmail.com>
2023-04-08 16:40:18 +0300

    refactor changelog, docker file. Delete .xml changelogs. Add TestContainer.

--- migrations/scripts/sql/2023-04-06-CRS-4_create_human_interests_table.sql
+++ migrations/scripts/sql/2023-04-06-CRS-4_create_human_interests_table.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset g.liseychikov:CRS-4_create_humans_interests_table
-CREATE TABLE IF NOT EXISTS humans_interests
+CREATE TABLE IF NOT EXISTS human_interests
 (
     chat_id         BIGINT      REFERENCES chats(id)        NOT NULL,
     link_id         BIGINT      REFERENCES links(id)        NOT NULL,
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS humans_interests
 );
 
 --changeset g.liseychikov:CRS-4_add_index_chat_link_id
-CREATE INDEX idx_chat_link_id ON humans_interests(chat_id, link_id);
+CREATE INDEX idx_chat_link_id ON human_interests(chat_id, link_id);
---
+++ scrapper/src/test/java/ru/tinkoff/edu/java/database/IntegrationEnvironment.java
@@ -0,0 +1,52 @@
+package ru.tinkoff.edu.java.database;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.core.PostgresDatabase;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.DirectoryResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+import lombok.SneakyThrows;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.sql.Connection;
+
+@Testcontainers
+public abstract class IntegrationEnvironment {
+    private static final Path SCRAPPER_PATH = new File(".").toPath().toAbsolutePath().getParent();
+    protected static final PostgreSQLContainer<?> DB_CONTAINER;
+
+    static {
+        DB_CONTAINER = new PostgreSQLContainer<>("postgres:15")
+                .withDatabaseName("scrapper")
+                .withUsername("tinkoff")
+                .withPassword("he_is_the_only_one");
+        DB_CONTAINER.start();
+        runMigrations();
+    }
+
+    @DynamicPropertySource
+    static void jdbsProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasourse.url", DB_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasourse.username", DB_CONTAINER::getUsername);
+        registry.add("spring.datasourse.password", DB_CONTAINER::getPassword);
+    }
+
+    @SneakyThrows
+    private static void runMigrations() {
+        try (Connection conn = DB_CONTAINER.createConnection("")) {
+            PostgresDatabase database = new PostgresDatabase();
+            database.setConnection(new JdbcConnection(conn));
+
+            ResourceAccessor changelogDir = new DirectoryResourceAccessor(SCRAPPER_PATH.getParent().resolve("migrations"));
+            Liquibase liquibase = new Liquibase("master.xml", changelogDir, database);
+            liquibase.update(new Contexts(), new LabelExpression());
+        }
+    }
+}
